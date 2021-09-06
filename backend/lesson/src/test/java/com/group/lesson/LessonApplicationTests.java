bde2941804f7ac84de9dedc7850042dd3cf5b088
ASUS <1195021547@qq.com>
2021-09-06 18:20:30 +0800

    更新了后端框架

---
+++ backend/lesson/src/test/java/com/group/lesson/LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
